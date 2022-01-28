dbab0a4dafc518fbb7d47d185cb7c6a4a060547d
Sanches536 <edsanches@hotmail.com>
2022-01-28 17:47:59 -0300

    Estado e Cidade

---
+++ src/main/java/com/sanches/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.sanches.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sanches.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+
+}
---
+++ src/main/java/com/sanches/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.sanches.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sanches.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}
