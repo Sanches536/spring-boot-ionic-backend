7bf0345b28e5306926d2e2210321829df0d13bd2
Sanches536 <edsanches@hotmail.com>
2022-01-29 15:01:30 -0300

    ItemPedido e itemPedidoPK

---
+++ src/main/java/com/sanches/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,11 @@
+package com.sanches.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sanches.cursomc.domain.ItemPedido;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+
+}
