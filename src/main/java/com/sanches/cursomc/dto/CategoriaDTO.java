e80d3babd6f280cf89f0d2deaf1f40ecfa3860d4
Sanches536 <edsanches@hotmail.com>
2022-01-30 11:36:05 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/sanches/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/sanches/cursomc/dto/CategoriaDTO.java
@@ -3,11 +3,17 @@ package com.sanches.cursomc.dto;
 import java.io.Serializable;
 
 import com.sanches.cursomc.domain.Categoria;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
 
 public class CategoriaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
+	
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Length(min = 5, max = 80, message = "O tamanho dever ser entre 5 e 80 caracteres")
 	private String nome;
 	
 	public CategoriaDTO() {
--- src/main/java/com/sanches/cursomc/services/CategoriaService.java
+++ src/main/java/com/sanches/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.sanches.cursomc.domain.Categoria;
+import com.sanches.cursomc.dto.CategoriaDTO;
 import com.sanches.cursomc.repositories.CategoriaRepository;
 import com.sanches.cursomc.services.exceptions.DataIntegrityException;
 import com.sanches.cursomc.services.exceptions.ObjectNotFoundException;
@@ -55,6 +56,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return categoriaRepository.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
 	
 	
